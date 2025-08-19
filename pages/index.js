5e81f56ce883e265c3e863dc9f93f2e91c1913b4
Amanda <Amanda13784@gmail.com>
2025-08-18 21:28:24 -0400

    Continues working with IFrame

--- pages/index.js
+++ pages/index.js
@@ -71,9 +71,9 @@ export default function Home() {
                         <div className="flex justify-center lg:justify-start">
                             <div className="w-full max-w-[600px]">
                                 <iframe
-                                    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61578349430825&tabs=timeline&width=340&height=900&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
+                                    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61578349430825&tabs=timeline&width=340&height=331&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
                                     width="100%"
-                                    height="900"
+                                    height="331"
                                     style={{ border: 'none', overflow: 'hidden' }}
                                     scrolling="no"
                                     frameBorder="0"
